e3dfa11a7fa832f589ddf75fb8705c256fdb71fd
faridho <faridho45@gmail.com>
2020-05-03 22:58:15 +0700

    full code

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Faridho on 26/04/20.
+//  Copyright © 2020 Faridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func getRatesClick(_ sender: Any) {
+        
+        // 3) Parsing & JSON Serialization
+        
+        // 1) Request & Session
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=733390483a81d1e237cb5eb36c104105")
+        let session  = URLSession.shared
+        
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                // 2) Response & Data
+                if data != nil {
+                    do {
+                        let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                        
+                        DispatchQueue.main.async {
+                            if let rates = jsonResult["rates"] as? [String: Any] {
+                                
+                                if let chf = rates["CHF"] as? Double {
+                                    self.chfLabel.text = "CHF: \(chf)"
+                                }
+                                
+                                if let gbp = rates["GBP"] as? Double {
+                                    self.gbpLabel.text = "GBP: \(gbp)"
+                                }
+                                
+                                if let jpy = rates["JPY"] as? Double {
+                                    self.jpyLabel.text = "JPY: \(jpy)"
+                                }
+                                
+                                if let usd = rates["USD"] as? Double {
+                                    self.usdLabel.text = "USD: \(usd)"
+                                }
+                                
+                                if let turkish = rates["TRY"] as? Double {
+                                    self.tryLabel.text = "TRY: \(turkish)"
+                                }
+                            }
+                        }
+                    } catch  {
+                        print("error")
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+}
+
